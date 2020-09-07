e1e4bb1b897a4fe2d0c2965661bbbfbcf5540c9c
Hyunsoo Kim <gustnqpf3@gmail.com>
2020-09-07 10:40:09 +0900

    Initial Commit

---
+++ LocalDataUse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalDataUse
+//
+//  Created by 김현수 on 07/09/2020.
+//  Copyright © 2020 Hyun Soo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
